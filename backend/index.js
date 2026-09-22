7c459cb8ef2643fae70ee05ae0aa594cf7d78673
paramjeet singh <paramjeet.sde@gmail.com>
2026-09-22 23:54:50 +0530

    url change i index file

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const server = http.createServer(app);
 //Initialize socket.io server
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://mern-chat-app-cnhl.onrender.com",
     credentials: true,
   },
 });
